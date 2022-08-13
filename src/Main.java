69f757ca579593b642099d317845f2cc1420305f
Domenico Celani <menico.c90@gmail.com>
2022-08-13 11:31:21 +0200

    terminate work

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Paramether p = new Paramether(6, -7, 1);
+        System.out.println(p);
+        System.out.println(p.delta());
+        p.valutationDelta();
+        p.calcoltor();
+
     }
 }
--- src/Paramether.java
+++ src/Paramether.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.sqrt;
+
 public class Paramether {
 
     private double a;
@@ -7,7 +9,7 @@ public class Paramether {
     public Paramether(double a, double b, double c){
         this.a=a;
         this.b=b;
-        this.c=c,
+        this.c=c;
     }
     public double delta(){
         double delta= b*b-4*a*c;
@@ -17,8 +19,22 @@ public class Paramether {
         if(delta() > 0)
             System.out.println("The parabolic is positive");
         else if (delta() < 0)
-            System.out.println("The parabolic is negative");
+            System.out.println("The parabolic no exist");
         else
             System.out.println("The parabolic is 0");
+    }
+
+    public void calcoltor(){
+        double x1= (-b+sqrt(delta()))/2*a;
+        double x2= (-b-sqrt(delta()))/2*a;
+        System.out.println("X 1 = "+x1);
+        System.out.println("X 2 = "+x2);
+    }
+
+    @Override
+    public String toString() {
+        return "-----Equazion-----" +"\n"+
+                a+"X^2"+"+"+b+"X"+c+"=0";
+
     }
 }
